e75145e67ba2f94e621f5081b057adacd2ccb3b8
Elsayed Abdullah <sayedaladway@gmail.com>
2022-11-17 13:39:35 +0200

    implementing login and transfer

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
 // BANKIST APP
 
 // Data
@@ -47,6 +45,7 @@ const labelTimer = document.querySelector('.timer');
 const containerApp = document.querySelector('.app');
 const containerMovements = document.querySelector('.movements');
 
+const formLogin = document.querySelector('.login');
 const btnLogin = document.querySelector('.login__btn');
 const btnTransfer = document.querySelector('.form__btn--transfer');
 const btnLoan = document.querySelector('.form__btn--loan');
@@ -79,8 +78,38 @@ function displayMovements(movements) {
   });
 }
 
-displayMovements(account1.movements);
+// function to display Balance
+const calcDisplayBalance = function (account) {
+  account.balance = account.movements.reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
+  labelBalance.textContent = `${account.balance} €`;
+};
 
+// function to display summary
+const calcDisplaySummary = function (account) {
+  const incomes = account.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = account.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = account.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * account.interestRate) / 100)
+    .filter(int => int >= 1)
+    .reduce((acc, mov) => acc + mov, 0);
+
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+// function to create username
 const createUsername = function (accs) {
   accs.forEach(acc => {
     return (acc.username = acc.owner
@@ -93,15 +122,71 @@ const createUsername = function (accs) {
 };
 
 createUsername(accounts);
-console.log(accounts);
 
-// let username = 'Steven Thomas Williams'
-//   .toLowerCase()
-//   .split(' ')
-//   .map(name => name[0])
-//   .join('');
-// // username.toLowerCase();
-// console.log(username);
+const updateUi = function (account) {
+  // display Movements
+  displayMovements(account.movements);
+
+  // display Balance
+  calcDisplayBalance(account);
+
+  // display Summary
+  calcDisplaySummary(account);
+};
+
+let currentAccount;
+
+// implementing login
+
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+
+  if (currentAccount?.pin === +inputLoginPin.value) {
+    // display ui and welcome message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+
+    // Clear Input Fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    containerApp.style.opacity = 1;
+
+    // update UI
+    updateUi(currentAccount);
+  }
+});
+
+// implementing transfer
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiveAccount = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+
+  // Clear Input Fields
+  inputTransferTo.value = inputTransferAmount.value = '';
+  inputTransferAmount.blur();
+
+  if (
+    receiveAccount &&
+    amount > 0 &&
+    currentAccount.balance >= amount &&
+    receiveAccount?.username !== currentAccount.username
+  ) {
+    // transfer
+    currentAccount.movements.push(-amount);
+    receiveAccount.movements.push(amount);
+
+    // update UI
+    updateUi(currentAccount);
+  }
+});
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -116,3 +201,40 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+/**
+ * 
+ * Let's go back to Julia and Kate's study about dogs. This time, they want to convert 
+dog ages to human ages and calculate the average age of the dogs in their study.
+Your tasks:
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's 
+ages ('ages'), and does the following things in order:
+1. Calculate the dog age in human years using the following formula: if the dog is 
+<= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, 
+humanAge = 16 + dogAge * 4
+2. Exclude all dogs that are less than 18 human years old (which is the same as 
+keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know 
+from other challenges how we calculate averages �)
+4. Run the function for both test datasets
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+ */
+/*
+const calcAverageHumanAge = ages => {
+  const humanAges = ages.map(age => (age <= 2 ? age * 2 : 16 + age * 4));
+  const adults = humanAges.filter(age => age >= 18);
+  const avg = adults.reduce((acc, cur) => acc + cur, 0);
+
+  return avg / adults.length;
+};
+const calcAverageHumanAge2 = ages =>
+  ages
+    .map(age => (age <= 2 ? age * 2 : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+console.log(calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge2([16, 6, 10, 5, 6, 1, 4]));
+*/
